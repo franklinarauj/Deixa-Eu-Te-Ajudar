2356b772fe63bcd7a81cc3ae23d15114631cc595
Matheus Eduardo Muniz <matheus.emuniz.dev@gmail.com>
2021-05-10 19:46:31 -0300

    Fonte Leo hates school

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,30 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  :root {
+    --primary: #83FFE6;
+    --secondary: #FF5F5F;
+  }
 
-#nav {
-  padding: 30px;
-}
+  @font-face {
+    font-family: Leo;
+    src: url("./assets/Leo hates school.ttf");
+  }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  #app {
+    font-family: "Leo";
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+  body {
+    min-height: 100vh;
+    background: var(--primary);
+  }
 </style>
---
+++ src/pages/Splash.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+
+    <p>
+      {{ texto }}
+    </p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Splash",
+
+    data() {
+      return {
+        texto: "aoegmaoegm",
+      };
+    },
+
+    methods: {
+      butao() {},
+    },
+  };
+</script>
