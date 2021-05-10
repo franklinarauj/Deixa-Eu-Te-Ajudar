3f473d83798083df08922188d69c6f8b2380c976
Matheus Eduardo Muniz <matheus.emuniz.dev@gmail.com>
2021-05-10 20:13:51 -0300

    Estrutura de roteamento.

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,13 +1,16 @@
 <template>
-  
+  <div>
+    <navbar />
+    <router-view />
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+  import Navbar from "../components/Navbar.vue";
+  export default {
+    components: { Navbar },
+  };
 </script>
 
 <style>
-
 </style>
---
+++ src/pages/Menu.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>XABLAU</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
